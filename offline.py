061176bd397eb7e677ed10e164f4f922ee0f6dc7
kuhung <kuhung@foxmail.com>
2020-04-14 20:33:54 +0800

    add img resize

--- offline.py
+++ offline.py
@@ -15,9 +15,18 @@ def gen_future(img_path):
     feature_path = 'static/feature/' + os.path.splitext(os.path.basename(img_path))[0] + '.pkl'
     pickle.dump(feature, open(feature_path, 'wb'))
 
+def resize_img(img_path,rate):
+    img = Image.open(img_path)  # PIL image
+    width, height = img.size
+    width = int(width*rate)
+    height = int(height*rate)
+    output_img = img.resize((width,height))
+    output_img.save(img_path)
+
 # normal
 if __name__ == "__main__":
     for img_path in tqdm(sorted(glob.glob('static/img/*.jpg'))):
+        # resize_img(img_path,0.7)
         gen_future(img_path)
 
 
